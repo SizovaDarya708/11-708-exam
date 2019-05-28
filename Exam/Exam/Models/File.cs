8103d46773886df81158c95ccb3d1619066f9326
Daryayan <daryasa1991@mail.ru>
2019-05-28 10:35:59 +0300

    app

---
+++ Exam/Exam/Models/File.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Exam.Models
+{
+    public class File
+    {
+        public int Id { get; set; }
+        public string ShortDescription { get; set; }
+        public string FullDescription { get; set; }
+        public string Link { get; set; }
+    
+    }
+}
